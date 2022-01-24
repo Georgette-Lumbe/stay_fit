539864a952cab9371f8b278fb2375892bebfc7bd
Georgette-Lumbe <lumbe.georgette@gmail.com>
2022-01-24 21:14:30 +0000

    Add product details functionality, add pr and add products images

--- products/models.py
+++ products/models.py
@@ -11,22 +11,23 @@ class Category(models.Model):
     friendly_name = models.CharField(max_length=260, null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
     def get_friendly_name(self):
-        return self.friendly_name
+        return self.friendly_name   # Display friendly name
 
 
 class Product(models.Model):
     """Product Model"""
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)  # noqa
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)  # noqa
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    objects = models.Manager()  # Object member
 
     def __str__(self):
-        return self.name
+        return str(self.name)
