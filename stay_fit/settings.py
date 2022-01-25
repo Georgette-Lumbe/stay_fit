c21299f60214a97ca0d807d707767ad048bb07f2
Georgette-Lumbe <lumbe.georgette@gmail.com>
2022-01-25 21:45:17 +0000

    Add delivery and update context processor

--- stay_fit/settings.py
+++ stay_fit/settings.py
@@ -155,6 +155,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+FREE_DELIVERY_THRESHOLD = 30
+STANDARD_DELIVERY_PERCENTAGE = 10
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
