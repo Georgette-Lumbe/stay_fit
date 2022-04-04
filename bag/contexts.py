957836d79c4e40fbb0884000e8f776640ab5bea0
Georgette-Lumbe <lumbe.georgette@gmail.com>
2022-04-04 10:14:40 +0000

    Update context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,6 +1,8 @@
 """ Import """
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
@@ -10,6 +12,42 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        elif 'items_by_size' in item_data:
+            product = get_object_or_404(Product, pk=item_id)
+            size = request.POST.get('product_size', False)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            shoesize = request.POST.get('product_shoesize', False)
+            for shoesize, quantity in item_data['items_by_shoesize'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'shoesize': shoesize,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
