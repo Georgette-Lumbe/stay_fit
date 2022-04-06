4f1681d0c0bda14df5157fdbdfa396e248ea8f5b
Georgette-Lumbe <lumbe.georgette@gmail.com>
2022-04-06 03:12:55 +0000

    Complete confirmation emails

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -85,6 +85,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.get(
                     full_name__iexact=shipping_details.name,
+                    user_profile=profile,
                     email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
                     country__iexact=shipping_details.address.country,
