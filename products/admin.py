8cb9e82983166086a76f01e3dbdaa81569e7a55f
Georgette-Lumbe <lumbe.georgette@gmail.com>
2022-06-02 16:01:49 +0000

    Add Review and Contact model in Admin Pannel

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,6 @@
 """Import"""
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Review
 
 # Register your models here.
 
@@ -30,3 +30,10 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('review_title', 'review', 'review_author', 'product')
+
+
+admin.site.register(Review, ReviewAdmin)
