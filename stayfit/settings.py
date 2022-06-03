9f40af1f30a6194a009f3a21d71de8ee1d41a972
Georgette-Lumbe <lumbe.georgette@gmail.com>
2022-06-03 08:29:05 +0000

    Set Debug to true for tracing the deployed site issue

--- stayfit/settings.py
+++ stayfit/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')  # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['stayfit2022.herokuapp.com', 'localhost']
 
